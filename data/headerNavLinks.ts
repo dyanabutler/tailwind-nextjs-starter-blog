a67b2ce0270485fc478f539211d75ef8c63ad7f7
Dyana Butler <dyanajbutler98@gmail.com>
2023-12-27 18:58:31 -0500

    md

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: 'https://dyanabutler.com/', title: 'Home' },
+  { href: '/', title: 'Back' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
 ]
 
